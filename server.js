f51fc341c18aef2ea91f299a8d5746b56c687278
김한들 <dle1883@gmail.com>
2024-03-07 14:50:07 +0900

    배포 수정

--- server.js
+++ server.js
@@ -11,7 +11,24 @@ app.use(cors())
 app.use(bodyParser.urlencoded({ extended: true }));
 
 //    /submit
-app.post('https://handeul-kim.github.io/yj/contact.html', async (req, res) => {
+
+
+
+
+
+
+app.get('/', (req,res) => {
+    res.sendFile(__dirname + '/contact.html')
+})
+
+
+
+
+
+
+
+
+app.post('/submit', async (req, res) => {
     console.log('데이터:', req.body);
     
     await sendEmail(req.body.name, req.body.email, req.body.tel, req.body.title, req.body.content)
